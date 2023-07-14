b699111da6c05cf2f395d4a70c73fc715bd26f25
Vasya <oiuy78@yandex.ru>
2023-07-14 05:40:43 +0300

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Test_task_for_Webtronics
 Simple RESTful API using FastAPI for a social networking application
+
+## Before start, set your settings in the .env file
+```
+SECRET_KEY=''  # random key
+
+TOKEN_AUDIENCE=''  # example jwt
+
+POSTGRES_ENGINE=postgresql
+POSTGRES_HOST=db
+POSTGRES_PORT=5432
+POSTGRES_NAME=postgres
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD='' db password
+
+DB_ENGINE_TEST=''  # test engine db
+DB_HOST_TEST=''  # test db host
+DB_PORT_TEST=''  # test db port
+DB_NAME_TEST=''  # test db name
+DB_USER_TEST=''  # test db user
+DB_PASSWORD_TEST=''  # test db password
+
+REDIS_HOST=redis
+REDIS_PORT=6379 
+
+CORS_ORIGIN=http://localhost:8000  # and your
+```
+# Very important!
++ **If the project does not up the first time, try again or delete the 'data' folder!**  
+# Start
+```commandline
+docker compose up --build
+```
+# Tests
+```commandline
+docker compose exec web pytest tests
+```
--- src/config.py
+++ src/config.py
@@ -4,35 +4,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = environ.get('SECRET_KEY', 'weffhwieugf@H#I2jrT$#tjheorhgeiurghierhgg54##R$%Tgreg')
 
 # Database
-DB_ENGINE = environ.get('POSTGRES_ENGINE')
-DB_HOST = environ.get('POSTGRES_HOST')
-DB_PORT = environ.get('POSTGRES_PORT')
-DB_NAME = environ.get('POSTGRES_NAME')
-DB_USER = environ.get('POSTGRES_USER')
-DB_PASSWORD = environ.get('POSTGRES_PASSWORD')
+DB_ENGINE = environ.get('POSTGRES_ENGINE', 'postgresql')
+DB_HOST = environ.get('POSTGRES_HOST', 'db')
+DB_PORT = environ.get('POSTGRES_PORT', 5432)
+DB_NAME = environ.get('POSTGRES_NAME', 'postgres')
+DB_USER = environ.get('POSTGRES_USER', 'postgres')
+DB_PASSWORD = environ.get('POSTGRES_PASSWORD', '123456')
 
 # Test database
-DB_ENGINE_TEST = environ.get('DB_ENGINE_TEST')
-DB_HOST_TEST = environ.get("DB_HOST_TEST")
-DB_PORT_TEST = environ.get("DB_PORT_TEST")
-DB_NAME_TEST = environ.get("DB_NAME_TEST")
-DB_USER_TEST = environ.get("DB_USER_TEST")
-DB_PASSWORD_TEST = environ.get("DB_PASSWORD_TEST")
+DB_ENGINE_TEST = environ.get('DB_ENGINE_TEST', 'postgresql')
+DB_HOST_TEST = environ.get("DB_HOST_TEST", 'db')
+DB_PORT_TEST = environ.get("DB_PORT_TEST", 5432)
+DB_NAME_TEST = environ.get("DB_NAME_TEST", 'postgres')
+DB_USER_TEST = environ.get("DB_USER_TEST", 'postgres')
+DB_PASSWORD_TEST = environ.get("DB_PASSWORD_TEST", '123456')
 
 # Auth
 JWT_LIFETIME = 10 * 60
 COOKIE_NAME = 'JWT'
-TOKEN_AUDIENCE = environ.get('TOKEN_AUDIENCE')
+TOKEN_AUDIENCE = environ.get('TOKEN_AUDIENCE', 'jwt')
 
 # REDIS
-REDIS_HOST = environ.get('REDIS_HOST')
-REDIS_PORT = environ.get('REDIS_PORT')
+REDIS_HOST = environ.get('REDIS_HOST', 'redis')
+REDIS_PORT = environ.get('REDIS_PORT', 6379)
 
 # CORS
-CORS_ORIGIN = environ.get('CORS_ORIGIN').split(' ')
+CORS_ORIGIN = environ.get('CORS_ORIGIN', 'http://localhost:8000').split(' ')
 ALLOW_METHODS = ["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"]
 ALLOW_HEADERS = ["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
                  "Access-Control-Allow-Origin", "Authorization"]
